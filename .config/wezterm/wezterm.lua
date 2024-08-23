cae7c5199e08c16ed2fafa120b8561e3b6f02a61
Ye Thatoe Kyaw <thatoe@pm.me>
2024-08-23 14:14:09 +0800

    updates

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require("wezterm")
+
+return {
+  color_scheme = "Gruvbox dark, hard (base16)",
+  font = wezterm.font("monospace"),
+  font_size = 11.0,
+  hide_tab_bar_if_only_one_tab = true,
+}
