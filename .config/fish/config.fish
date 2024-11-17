5dfa7212553b34efaf20d3c75a7772addd172db3
Ye Thatoe Kyaw <thatoe@pm.me>
2024-11-17 16:18:00 +0800

    .

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,11 @@
 if status is-interactive
   zoxide init fish --cmd cd | source
   starship init fish | source
+  fish_vi_key_bindings
 end
 
 if status is-login
-  if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-    exec startx -- -keeptty
+  if test -z "$WAYLAND_DISPLAY" -a "$XDG_VTNR" = 1
+    exec Hyprland
   end
 end
