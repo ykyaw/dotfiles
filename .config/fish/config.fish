c1ee171ab668cb8b61f6a05113ab1dcbae38750b
Ye Thatoe Kyaw <thatoe@pm.me>
2025-01-29 12:49:52 +0800

    .

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,21 @@
+set -x EDITOR nvim
+set -x VISUAL nvim
+set -x CHROME_EXECUTABLE chromium
+fish_add_path -gp ~/development/flutter/bin
+fish_add_path -g ~/.pub-cache/bin
+
 if status is-interactive
-  zoxide init fish --cmd cd | source
-  starship init fish | source
   fish_vi_key_bindings
+  starship init fish | source
+  zoxide init fish --cmd cd | source
+  fzf --fish | source
+
+  alias ls=lsd
+  alias cat=bat
 end
 
 if status is-login
-  if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-    exec startx
+  if test -z "$WAYLAND_DISPLAY" -a "$XDG_VTNR" = 1
+    exec Hyprland
   end
 end
